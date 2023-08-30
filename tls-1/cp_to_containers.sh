ee4825c03f778c405afd1b6032313aa4ae8ad169
JimMoen <LnJimMoen@outlook.com>
2023-08-30 16:20:09 +0800

    feat: auto emqx cluster

--- tls-1/cp_to_containers.sh
+++ tls-1/cp_to_containers.sh
@@ -3,3 +3,15 @@
 docker cp emqx_backend_hstreamdb.conf emqx-4420-bmw-0:/opt/emqx/etc/plugins/;
 docker cp emqx_backend_hstreamdb.conf emqx-4420-bmw-1:/opt/emqx/etc/plugins/;
 docker cp emqx_backend_hstreamdb.conf emqx-4420-bmw-2:/opt/emqx/etc/plugins/;
+
+docker cp hstream.crt emqx-4420-bmw-0:/opt/emqx/etc/certs/;
+docker cp hstream.crt emqx-4420-bmw-1:/opt/emqx/etc/certs/;
+docker cp hstream.crt emqx-4420-bmw-2:/opt/emqx/etc/certs/;
+
+docker cp hstream.key emqx-4420-bmw-0:/opt/emqx/etc/certs/;
+docker cp hstream.key emqx-4420-bmw-1:/opt/emqx/etc/certs/;
+docker cp hstream.key emqx-4420-bmw-2:/opt/emqx/etc/certs/;
+
+docker cp root_ca.crt emqx-4420-bmw-0:/opt/emqx/etc/certs/;
+docker cp root_ca.crt emqx-4420-bmw-1:/opt/emqx/etc/certs/;
+docker cp root_ca.crt emqx-4420-bmw-2:/opt/emqx/etc/certs/;
